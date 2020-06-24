bb69ab1dfcb6861bd73ee5729c2ce64e0095c55a
Ajay Rana <ajay.singh.84@gmail.com>
2020-06-24 15:22:23 +0530

    test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,42 +12,6 @@ public class QueryProcessor {
         	Integer t = Integer.parseInt(parts[3]) + Integer.parseInt(parts[3]);
         	return t.toString();
         }
-        if (query.toLowerCase().contains("what is 4 plus 16")) {
-            return "20";
-        }
-        if (query.toLowerCase().contains("what is 6 plus 0")) {
-            return "6";
-        }
-        if (query.toLowerCase().contains("what is 15 plus 5")) {
-            return "20";
-        }
-        if (query.toLowerCase().contains("which of the following numbers is the largest: 133, 25")) {
-            return "158";
-        }
-        if (query.toLowerCase().contains("which of the following numbers is the largest: 49, 183, 60, 630")) {
-            return "630";
-        }
-        if (query.toLowerCase().contains("what is 5 plus 19")) {
-            return "24";
-        }
-        if (query.toLowerCase().contains("what is 15 plus 17")) {
-            return "22";
-        }
-        if (query.toLowerCase().contains("what is 16 plus 2")) {
-            return "18";
-        }
-        if (query.toLowerCase().contains("what is 13 plus 13")) {
-            return "26";
-        }
-        if (query.toLowerCase().contains("which of the following numbers is the largest: 56, 641")) {
-            return "641";
-        }
-        if (query.toLowerCase().contains("which of the following numbers is the largest: 15, 11, 590, 427")) {
-            return "590";
-        }
-        if (query.toLowerCase().contains("what is 5 plus 19")) {
-            return "24";
-        }
         return "";
     }
 }
